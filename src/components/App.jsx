247392be3e40bd11191e9e954fd819b34237035f
IgorMushk <igor.mushketik@gmail.com>
2023-09-05 22:17:06 +0300

    Header + Modal - in App doing class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,37 @@
-import { Container } from "./App.styled";
-import Counter from "./Counter/Counter";
-import Header from "./Header/Header";
-import Modal from "./Modal/Modal";
+import { Component } from 'react';
+import { Container } from './App.styled';
+import Counter from './Counter/Counter';
+import Header from './Header/Header';
+import Modal from './Modal/Modal';
 
-export const App = () => {
-  return (
-    <>
-    <Container>
-      React homework template!
-    </Container>
-    <Header />
-    <Counter/>
-    <Modal>Modal</Modal>
-    </>
-  );
-};
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <Container>React homework template!</Container>
+        <Header />
+        <Counter />
+        <Modal>Text for Modal</Modal>
+      </>
+    );
+  }
+}
 
+export default App;
+
+// export const App = () => {
+//   return (
+//     <>
+//     <Container>
+//       React homework template!
+//     </Container>
+//     <Header />
+//     <Counter/>
+//     <Modal>Modal</Modal>
+//     </>
+//   );
+// };
 
 // export const App = () => {
 //   return (
