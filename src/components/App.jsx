e5f936f05f0fcc7cfb24950ff038d50ba415f53b
IgorMushk <igor.mushketik@gmail.com>
2023-09-05 17:08:03 +0300

    Counter - start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
+import { Container } from "./App.styled";
+import Counter from "./Counter/Counter";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <>
+    <Container>
       React homework template!
-    </div>
+    </Container>
+    <Counter>
+      
+    </Counter>
+    </>
   );
 };
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template!
+//     </div>
+//   );
+// };
