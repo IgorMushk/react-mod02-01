aefe0c6d70f07045314b319f863b51d4e6434d66
IgorMushk <igor.mushketik@gmail.com>
2023-09-05 21:02:49 +0300

    Counter - классовый подход - State - классовый компонент
    Из обычного компонента и переписали на классовый

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,41 +1,53 @@
-const Counter = () => {
-    // const handleClick = (evt) => (
-    //     console.log('evt', evt)
-    // )
-    let counter = 0
-    const handleClick = () => {
-        counter++;
-        console.log('counter', counter)
+import { Component } from "react";
+
+class Counter extends Component{
+    //constructor()
+    state = {
+        counter: 0,
+        items: [],
     }
-	return (
-		<div className='position-absolute top-50 start-50 translate-middle'>
-			<div
-				className='card bg-dark text-white '
-				style={{ width: '600px' }}
-			>
-				<div className='card-body'>
-					<h5 className='card-title text-center fs-1'>Counter</h5>
-					<p
-						className='card-text  text-center'
-						style={{ fontSize: '80px' }}
-					>
-					{counter}
-                    </p>
-					<div className='d-flex justify-content-center px-5'>
-						<button onClick={handleClick}
-							className='btn btn-outline-success me-5'
-						>
-							<i className='bi bi-plus-circle fs-1'></i>
-						</button>
-						<button className='btn  btn-outline-danger ms-5'>
-							<i className='bi bi-dash-circle fs-1'></i>
-						</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+
+    handleClick = () => {
+           // console.log('this.state', this.state)
+           //this.state.counter++
+           this.setState({
+            counter: 10,
+            items: [123,123],
+           })
+           console.log('state :', this.state);
+    }
+
+    render() {
+        console.log('state 2:', this.state);
+        return <div className='position-absolute top-50 start-50 translate-middle'>
+        <div
+            className='card bg-dark text-white '
+            style={{ width: '600px' }}
+        >
+            <div className='card-body'>
+                <h5 className='card-title text-center fs-1'>Counter</h5>
+                <p
+                    className='card-text  text-center'
+                    style={{ fontSize: '80px' }}
+                >
+                {this.state.counter}
+                </p>
+                <div className='d-flex justify-content-center px-5'>
+                    <button onClick={this.handleClick}
+                        className='btn btn-outline-success me-5'
+                    >
+                        <i className='bi bi-plus-circle fs-1'></i>
+                    </button>
+                    <button className='btn  btn-outline-danger ms-5'>
+                        <i className='bi bi-dash-circle fs-1'></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    }
 }
+
 
 export default Counter
 
