14a9aa0d00064c68a842b5bf449980ba663f4e2c
IgorMushk <igor.mushketik@gmail.com>
2023-09-05 21:27:08 +0300

    Counter - finish

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -7,23 +7,31 @@ class Counter extends Component{
         items: [],
     }
 
-    handleClick = () => {
-           // console.log('this.state', this.state)
-           //this.state.counter++
+    // handleClick = () => {
+    //        // console.log('this.state', this.state)
+    //        //this.state.counter++
 
-        //    this.setState({
-        //     counter: 10,
-        //     items: [123,123],
-        //    })
+    //     //    this.setState({
+    //     //     counter: 10,
+    //     //     items: [123,123],
+    //     //    })
 
-        // this.setState((prevState)=>{
-        //     //console.log('prevState', prevState);
-        //     return {counter:prevState.counter+1}
-        // })
-        //console.log('state :', this.state);
+    //     // this.setState((prevState)=>{
+    //     //     //console.log('prevState', prevState);
+    //     //     return {counter:prevState.counter+1}
+    //     // })
+    //     //console.log('state :', this.state);
 
+    //     this.setState((prevState)=> ({counter:prevState.counter+1}))
+    // }
+    
+    handleClickIncrement = () => {
         this.setState((prevState)=> ({counter:prevState.counter+1}))
-    } 
+    }
+    handleClickDecrement = () => {
+        this.setState((prevState)=> ({counter:prevState.counter-1}))
+    }
+    
 
     render() {
         //console.log('state 2:', this.state);
@@ -41,12 +49,12 @@ class Counter extends Component{
                 {this.state.counter}
                 </p>
                 <div className='d-flex justify-content-center px-5'>
-                    <button onClick={this.handleClick}
+                    <button onClick={this.handleClickIncrement}
                         className='btn btn-outline-success me-5'
                     >
                         <i className='bi bi-plus-circle fs-1'></i>
                     </button>
-                    <button className='btn  btn-outline-danger ms-5'>
+                    <button onClick={this.handleClickDecrement} className='btn  btn-outline-danger ms-5'>
                         <i className='bi bi-dash-circle fs-1'></i>
                     </button>
                 </div>
